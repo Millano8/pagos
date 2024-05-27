2e93a6fc504e239d63825cbd86ada628ef441e92
Millano <fran.millan95@gmail.com>
2024-05-27 17:51:23 -0300

    cleaning code

--- pages/api/auth/index.ts
+++ pages/api/auth/index.ts
@@ -1,6 +1,4 @@
 import {NextApiRequest,NextApiResponse} from "next"
-import {authMiddleware} from "lib/middlewares"
-import { User } from "lib/user"
 import {sendCode} from "lib/controllers/auth"
 
 export default async function (req:NextApiRequest,res:NextApiResponse, token){
